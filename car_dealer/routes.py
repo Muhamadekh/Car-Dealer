cd220567ed432047b7ea22c64ed119aae165e085
unknown <muhamadekhhaji99@gmail.com>
2023-03-30 00:25:41 +0300

    bugfix

--- car_dealer/routes.py
+++ car_dealer/routes.py
@@ -235,30 +235,35 @@ def hire_car():
     return render_template('hire_car.html', cars=cars, brand_list=brand_list, model_list=model_list, fuel_list=fuel_list )
 
 
-@app.route('/admin/<int:car_id>/car_for_hire', methods=['GET', 'POST'])
+@app.route('/<int:car_id>/car_for_hire', methods=['GET', 'POST'])
 def car_hire_info(car_id):
     car_for_hire = LendCar.query.get_or_404(car_id)
 
     return render_template('car_hire_details.html', car_for_hire=car_for_hire)
 
 
-@app.route('/admin/<int:car_id>/car_for_sale', methods=['GET', 'POST'])
+@app.route('/<int:car_id>/car_for_sale', methods=['GET', 'POST'])
 def car_sales_info(car_id):
     car_for_sale = Car.query.get_or_404(car_id)
 
     return render_template('car_sales_details.html', car_for_sale=car_for_sale)
 
 
-@app.route('/approve_car<int:car_id>', methods=['GET', 'POST'])
-def approve_car(car_id):
+@app.route('/approve_car<int:car_id>/car_for_sale', methods=['GET', 'POST'])
+def approve_car_for_sale(car_id):
     car_for_sale = Car.query.get_or_404(car_id)
+    car_for_sale.is_approved = True
+    db.session.add(car_for_sale)
+    db.session.commit()
+    flash("You have confirmed this car", "success")
+    return redirect(url_for('admin'))
+
+
+@app.route('/approve_car<int:car_id>/car_for_hire', methods=['GET', 'POST'])
+def approve_car_for_hire(car_id):
     car_for_hire = LendCar.query.get_or_404(car_id)
-    if car_for_sale:
-        car_for_sale.is_approved = True
-        db.session.add(car_for_sale)
-    if car_for_hire:
-        car_for_hire.is_approved = True
-        db.session.add(car_for_hire)
+    car_for_hire.is_approved = True
+    db.session.add(car_for_hire)
     db.session.commit()
     flash("You have confirmed this car", "success")
     return redirect(url_for('admin'))

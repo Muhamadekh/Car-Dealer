a40b0f11e16f17ef42bccab403dc08a7ebcf47ef
unknown <muhamadekhhaji99@gmail.com>
2023-05-23 18:54:07 +0300

    Changes after reverting

--- car_dealer/__init__.py
+++ car_dealer/__init__.py
@@ -1,3 +1,4 @@
+import os
 import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -7,17 +8,17 @@ from flask_migrate import Migrate
 from flask_mail import Mail
 
 
-# with open('/etc/config.json') as config_file:
-# 	config = json.load(config_file)
+with open('/etc/config.json') as config_file:
+	config = json.load(config_file)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ca756c5133bfd45cb5f1ff0c7a21d624'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///project.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = config.get('SQLALCHEMY_DATABASE_URI')
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USERNAME'] = config.get('EMAIL_USER')
-# app.config['MAIL_PASSWORD'] = config.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = config.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = config.get('EMAIL_PASS')
 
 
 db = SQLAlchemy(app)

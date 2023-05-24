5b031665a86115e7b3c526c7fd307c535999d4c8
unknown <muhamadekhhaji99@gmail.com>
2023-05-24 21:14:17 +0300

    Images slide show

--- car_dealer/static/js/custom.js
+++ car_dealer/static/js/custom.js
@@ -340,3 +340,28 @@ $("#carBrand").on("input",(e)=> {
 hireDropDown()
 })
 
+
+var slideIndex = 0;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+      var slides = document.getElementsByClassName("mySlides");
+
+      if (n >= slides.length) {
+        slideIndex = 0;
+      }
+
+      if (n < 0) {
+        slideIndex = slides.length - 1;
+      }
+
+      for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+
+      slides[slideIndex].style.display = "block";
+    }
